257b2378a47437fc8ae567ae4ae7c8f4cc57424e
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-06-05 18:06:27 +0200

    add coverage for mitm ca

---
+++ serve/ca_test.go
@@ -0,0 +1,15 @@
+package serve
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignHost(t *testing.T) {
+		ca, err := NewCA()
+		assert.NoError(t, err)
+		mitm, err := ca.Sign("anything")
+		assert.NoError(t, err)
+		assert.NotNil(t, mitm)
+}

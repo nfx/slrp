1d68be05c1d6ab48f0f4ab6c70245dc2cb2e44d1
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-06-06 19:21:27 +0200

    rudimentary test for sort strategies

---
+++ pool/sort_strategy_test.go
@@ -0,0 +1,26 @@
+package pool
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSortStrategyPicks(t *testing.T) {
+		entries := []entry{
+			{
+				ReanimateAfter: now(),
+				LastSeen: now().Unix(),
+				Speed: 5*time.Second,
+				Offered: 1,
+			},
+			{
+				ReanimateAfter: now(),
+				LastSeen: now().Unix(),
+				Speed: 1*time.Second,
+				Offered: 50,
+			},
+		}
+		fastestUnseen(entries)
+		sortForDisplay(entries)
+		saturate(entries)
+}

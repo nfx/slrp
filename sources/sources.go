f8402011fd22a2ce1625d0cd20ba4d1b87227a3c
doomedraven <doomedraven@users.noreply.github.com>
2022-12-25 13:21:32 +0100

    Update sources.go

--- sources/sources.go
+++ sources/sources.go
@@ -54,6 +54,18 @@ func init() {
 		Homepage:  "https://free-proxy-list.net/uk-proxy.html",
 		Frequency: 30 * time.Minute,
 		Feed:      httpProxyRegexFeed("https://free-proxy-list.net/uk-proxy.html", "UK Proxy List"),
+	}, Source{
+		ID:        55,
+		Homepage:  "https://www.proxy-list.download/",
+		Frequency: 3 * time.Hour,
+		Seed:      true,
+		Feed:      proxyListDownload,
+	}, Source{
+		ID:        56,
+		Homepage:  "https://www.freeproxychecker.com",
+		Frequency: 3 * time.Hour,
+		Seed:      true,
+		Feed:      freeproxychecker,
 	})
 }
 
@@ -68,4 +80,21 @@ func proxylists(ctx context.Context, h *http.Client) Src {
 		refresh(f("/http_highanon.txt", "http")).
 		refresh(f("/socks4.txt", "socks4")).
 		refresh(f("/socks5.txt", "socks5"))
+}
+
+func proxyListDownload(ctx context.Context, h *http.Client) Src {
+	f := regexFeedBase(ctx, h, "https://www.proxy-list.download/api/v1/get?type=", ":")
+	return merged().
+		refresh(f("http", "http")).
+		refresh(f("https", "https")).
+		refresh(f("socks4", "socks4")).
+		refresh(f("socks5", "socks5"))
+}
+
+func freeproxychecker(ctx context.Context, h *http.Client) Src {
+	f := regexFeedBase(ctx, h, "https://www.freeproxychecker.com/result/", ":")
+	return merged().
+		refresh(f("socks4_proxies.txt", "socks4")).
+		refresh(f("socks5_proxies.txt", "socks5")).
+		refresh(f("http_proxies.txt", "http"))
 }

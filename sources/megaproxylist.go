4fd6109e3bf8e1692247683c372e143e6f2611e8
doomedraven <doomedraven@users.noreply.github.com>
2022-12-25 21:46:16 +0100

    Update megaproxylist.go

--- sources/megaproxylist.go
+++ sources/megaproxylist.go
@@ -43,7 +43,8 @@ func Megaproxylist(ctx context.Context, h *http.Client) (found []pmux.Proxy, err
 	r := csv.NewReader(bytes.NewBuffer(csvData))
 	r.Comma = ';'
 	r.TrimLeadingSpace = true
-	// trick to skip header
+
+    // trick to skip header
 	if _, err := r.Read(); err != nil {
 		return found, nil
 	}

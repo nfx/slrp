e77a31f1870e36fe6f81701d605647bccabf76c1
Andriy Brukhovetskyy <doommedraven@gmail.com>
2022-12-27 13:04:32 +0100

    Update megaproxylist_test.go

--- sources/megaproxylist_test.go
+++ sources/megaproxylist_test.go
@@ -25,7 +25,7 @@ func TestMegaproxylist(t *testing.T) {
 func Test_unzipInMemory(t *testing.T) {
 	rFile, _ := os.ReadFile("./testdata/megaproxylist/test.zip")
 	data, _ := unzipInMemory(rFile)
-	assert.Equal(t, 100, len(data))
+	assert.Equal(t, 126, len(data))
 }
 
 func Test_readZipFile(t *testing.T) {
@@ -33,4 +33,32 @@ func Test_readZipFile(t *testing.T) {
 	zipReader, _ := zip.NewReader(bytes.NewReader(rFile), int64(len(rFile)))
 	fmt.Println(zipReader)
 	assert.Equal(t, zipReader.File[0].Name, "megaproxylist.csv", "Testing unzip in memory")
+}
+
+func Test_getIpAddr(t *testing.T) {
+	tests := map[string]struct {
+		input string
+		expected string
+	}{
+		`empty`: {
+			input: "",
+			expected: "",
+		},
+		`localhost`: {
+			input: "localhost",
+			expected: "127.0.0.1",
+		},
+		`localhostAsIp`: {
+			input: "127.0.0.1",
+			expected: "127.0.0.1",
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := getIpAddr(tt.input)
+			if got != tt.expected {
+				t.Errorf("Got: %s, expected: %s", got, tt.expected)
+			}
+		})
+	}
 }

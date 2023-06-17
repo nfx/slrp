c6c40aa4de90b929d91554a1bd3b269b7f906eec
Ar-Kan <arquimedes_macedo@hotmail.com>
2023-06-17 14:21:05 -0300

    Prettier first execution

--- ui/src/Reverify.js
+++ ui/src/Reverify.js
@@ -1,49 +1,66 @@
-import { IconHeader, LiveFilter, QueryFacets, useTitle, http } from './util'
-import { Countries } from './countries';
-import { useState } from 'react';
+import { IconHeader, LiveFilter, QueryFacets, useTitle, http } from "./util";
+import { Countries } from "./countries";
+import { useState } from "react";
 
-function Item({Proxy, Sources, Provider, ASN, Country, Attempt}) {
+function Item({ Proxy, Sources, Provider, ASN, Country, Attempt }) {
   const removeProxy = e => {
-    http.delete(`/blacklist/${Proxy.replace("//", '')}`)
-    return false
-  }
-  return <tr>
-    <td>
-      <a href='#remove' onClick={removeProxy}>x</a>
-    </td>
-    <td className='proxy'>
-    {Proxy} <sup className='text-muted' title={Sources.join(', ')}>{Sources.length} sources</sup>
-      {/* <small className='text-muted'>{Sources.join(', ')}</small> */}
-    </td>
-    <td className='country text-muted' title={Countries[Country]?.name}>{Countries[Country]?.flag}</td>
-    <td className='provider text-muted'>
-      <a href={`https://ipasn.com/asn-downstreams/${ASN}`} rel='noreferrer' target='_blank'>{Provider}</a>
-    </td>
-    <td className='attempt text-muted'>{Attempt}</td>
-  </tr>
+    http.delete(`/blacklist/${Proxy.replace("//", "")}`);
+    return false;
+  };
+  return (
+    <tr>
+      <td>
+        <a href="#remove" onClick={removeProxy}>
+          x
+        </a>
+      </td>
+      <td className="proxy">
+        {Proxy}{" "}
+        <sup className="text-muted" title={Sources.join(", ")}>
+          {Sources.length} sources
+        </sup>
+        {/* <small className='text-muted'>{Sources.join(', ')}</small> */}
+      </td>
+      <td className="country text-muted" title={Countries[Country]?.name}>
+        {Countries[Country]?.flag}
+      </td>
+      <td className="provider text-muted">
+        <a href={`https://ipasn.com/asn-downstreams/${ASN}`} rel="noreferrer" target="_blank">
+          {Provider}
+        </a>
+      </td>
+      <td className="attempt text-muted">{Attempt}</td>
+    </tr>
+  );
 }
 
 export default function Reverify() {
-  useTitle("Reverify")
+  useTitle("Reverify");
   const [result, setResult] = useState(null);
-  return <div className="card blacklist table-responsive">
-    <LiveFilter endpoint="/reverify" onUpdate={setResult} minDelay={10000} />
-    {result != null && <div>
-      <QueryFacets endpoint="/reverify" {...result} />
-      <table className='table text-start table-sm'>
-        <thead>
-          <tr className="text-uppercase text-muted">
-            <td />
-            <IconHeader icon="link proxy" title="Proxy used" />
-            <IconHeader icon="flag country" title="Country" />
-            <IconHeader icon="hdd-network provider" title="Provider" />
-            <IconHeader icon="hdd-network attempt" title="Attempt" />
-          </tr>
-        </thead>
-        <tbody>
-          {result.Records.map(r => <Item key={r.Proxy} {...r} />)}
-        </tbody>
-      </table>
-    </div>}
-  </div>
+  return (
+    <div className="card blacklist table-responsive">
+      <LiveFilter endpoint="/reverify" onUpdate={setResult} minDelay={10000} />
+      {result != null && (
+        <div>
+          <QueryFacets endpoint="/reverify" {...result} />
+          <table className="table text-start table-sm">
+            <thead>
+              <tr className="text-uppercase text-muted">
+                <td />
+                <IconHeader icon="link proxy" title="Proxy used" />
+                <IconHeader icon="flag country" title="Country" />
+                <IconHeader icon="hdd-network provider" title="Provider" />
+                <IconHeader icon="hdd-network attempt" title="Attempt" />
+              </tr>
+            </thead>
+            <tbody>
+              {result.Records.map(r => (
+                <Item key={r.Proxy} {...r} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
--- ui/src/util.js
+++ ui/src/util.js
@@ -1,81 +1,90 @@
-import axios from 'axios'
-import {useEffect, useState, useRef, Component, useCallback} from 'react';
-import { useSearchParams } from 'react-router-dom';
+import axios from "axios";
+import { useEffect, useState, useRef, Component, useCallback } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const http = axios.create({
-	baseURL: "/api",
+  baseURL: "/api"
 });
 
 export class ErrorBoundary extends Component {
-	state = { error: false, errorMessage: "" };
+  state = { error: false, errorMessage: "" };
 
-	static getDerivedStateFromError(error) {
-		return { error: true, errorMessage: error.toString() }
-	};
+  static getDerivedStateFromError(error) {
+    return { error: true, errorMessage: error.toString() };
+  }
 
-	componentDidCatch(error, _) {
-		console.error(error)
-	}
+  componentDidCatch(error, _) {
+    console.error(error);
+  }
 
-	render() {
-		if (this.state.error) {
-			return <div className="alert alert-danger" role="alert">
-				<h4 className="alert-heading">Failed</h4>
-				{this.state.errorMessage}
-			</div>
-		}
-		return this.props.children
-	}
+  render() {
+    if (this.state.error) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          <h4 className="alert-heading">Failed</h4>
+          {this.state.errorMessage}
+        </div>
+      );
+    }
+    return this.props.children;
+  }
 }
 
-export function TimeDiff({ts, title}) {
-  const now = new Date()
-  const it = new Date(ts)
-  let elapsed = Math.abs((now - it) / 1000)
-  let value = '?'
+export function TimeDiff({ ts, title }) {
+  const now = new Date();
+  const it = new Date(ts);
+  let elapsed = Math.abs((now - it) / 1000);
+  let value = "?";
   if (elapsed < 60) {
-    value = `${elapsed.toFixed()}s`
-  } else if (elapsed < 60*60) {
-    value = `${(elapsed/60).toFixed()}m`
-  } else if (elapsed < 60*60*24) {
-    value = `${(elapsed/60/60).toFixed()}h`
-  } else if (elapsed < 60*60*24*7) {
-    value = `${(elapsed/60/60/24).toFixed()}d`
-  } else if (elapsed < 60*60*24*30) {
-    value = `${(elapsed/60/60/24/7).toFixed()}w`
+    value = `${elapsed.toFixed()}s`;
+  } else if (elapsed < 60 * 60) {
+    value = `${(elapsed / 60).toFixed()}m`;
+  } else if (elapsed < 60 * 60 * 24) {
+    value = `${(elapsed / 60 / 60).toFixed()}h`;
+  } else if (elapsed < 60 * 60 * 24 * 7) {
+    value = `${(elapsed / 60 / 60 / 24).toFixed()}d`;
+  } else if (elapsed < 60 * 60 * 24 * 30) {
+    value = `${(elapsed / 60 / 60 / 24 / 7).toFixed()}w`;
   }
-	if (it > now) {
-		return <sup className='text-muted' title={title}>in {value}</sup>
-	}
-  return <sup className='text-muted' title={title}>{value} ago</sup>
+  if (it > now) {
+    return (
+      <sup className="text-muted" title={title}>
+        in {value}
+      </sup>
+    );
+  }
+  return (
+    <sup className="text-muted" title={title}>
+      {value} ago
+    </sup>
+  );
 }
 
 export function IconHeader({ icon, col, title }) {
-  const cl = `bi bi-${icon}`
-  return <th className={`col-${col}`}><i className={cl} title={title} /></th>
+  const cl = `bi bi-${icon}`;
+  return (
+    <th className={`col-${col}`}>
+      <i className={cl} title={title} />
+    </th>
+  );
 }
 
 export function Card({ label, value, increment = 0 }) {
-	return (
-		<div className="col">
-			<div className="card mb-3">
-				<div className="card-body">
-					<div className="row align-items-center gx-0">
-						<div className="col">
-							<h6 className="text-uppercase text-muted mb-2">
-								{label}
-							</h6>
-							<span className="h2 mb-0 ">
-								{value}
-							</span>
-							{increment > 0 &&
-								<span className="badge bg-success ms-2">+{increment}</span>}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="col">
+      <div className="card mb-3">
+        <div className="card-body">
+          <div className="row align-items-center gx-0">
+            <div className="col">
+              <h6 className="text-uppercase text-muted mb-2">{label}</h6>
+              <span className="h2 mb-0 ">{value}</span>
+              {increment > 0 && <span className="badge bg-success ms-2">+{increment}</span>}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export function LiveFilter({ endpoint, onUpdate, minDelay = 5000 }) {
@@ -86,134 +95,157 @@ export function LiveFilter({ endpoint, onUpdate, minDelay = 5000 }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
   let doFilter = useCallback(() => {
-    clearTimeout(savedCallback.current)
+    clearTimeout(savedCallback.current);
     savedCallback.current = setTimeout(() => {
-      http.get(endpoint, {params: searchParams}).then(response => {
-        if (pause) {
-          clearTimeout(savedCallback.current)
-          return
-        }
-        setTotal(response.data.Total)
-        onUpdate(response.data)
-        setFailure(null)
-        savedCallback.current = setTimeout(doFilter, minDelay)
-      }).catch(err => {
-        if (err.isAxiosError) {
-          setFailure(err.response.data.Message)
-        }
-        clearTimeout(savedCallback.current)
-        return false
-      })
-    }, 500)
-  }, [pause, savedCallback, searchParams, endpoint, minDelay, onUpdate])
+      http
+        .get(endpoint, { params: searchParams })
+        .then(response => {
+          if (pause) {
+            clearTimeout(savedCallback.current);
+            return;
+          }
+          setTotal(response.data.Total);
+          onUpdate(response.data);
+          setFailure(null);
+          savedCallback.current = setTimeout(doFilter, minDelay);
+        })
+        .catch(err => {
+          if (err.isAxiosError) {
+            setFailure(err.response.data.Message);
+          }
+          clearTimeout(savedCallback.current);
+          return false;
+        });
+    }, 500);
+  }, [pause, savedCallback, searchParams, endpoint, minDelay, onUpdate]);
 
   useEffect(() => {
-    doFilter()
-    return () => clearTimeout(savedCallback.current)
+    doFilter();
+    return () => clearTimeout(savedCallback.current);
   }, [savedCallback, doFilter]);
 
-  let filter = searchParams.get("filter") || ""
+  let filter = searchParams.get("filter") || "";
   let change = event => {
-    filter = event.target.value
-    setSearchParams(filter === "" ? {} : { filter })
-    doFilter()
-  }
+    filter = event.target.value;
+    setSearchParams(filter === "" ? {} : { filter });
+    doFilter();
+  };
 
   let togglePause = () => {
-    setPause(!pause)
+    setPause(!pause);
     if (pause) {
-      doFilter()
+      doFilter();
     } else {
-      clearTimeout(savedCallback.current)
+      clearTimeout(savedCallback.current);
     }
-  }
+  };
 
   return (
-    <div className='search-filter'>
-      <div className='input-group'>
+    <div className="search-filter">
+      <div className="input-group">
         <div>
-          {total != null && <span className='total'>{total} total</span>}
-          <input className="form-control form-control-dark w-100 border-secondary" type="text"
-                 value={filter} onChange={change} placeholder="Search" aria-label="Search"/>
+          {total != null && <span className="total">{total} total</span>}
+          <input className="form-control form-control-dark w-100 border-secondary" type="text" value={filter} onChange={change} placeholder="Search" aria-label="Search" />
         </div>
-        <button className="btn btn-outline-secondary border-secondary" type="button"
-                onClick={togglePause} title={!pause ? "Pause live update" : "Resume live update"}>
-          <i className={`bi ${pause ? 'bi-play' : 'bi-pause'}`}/>
+        <button className="btn btn-outline-secondary border-secondary" type="button" onClick={togglePause} title={!pause ? "Pause live update" : "Resume live update"}>
+          <i className={`bi ${pause ? "bi-play" : "bi-pause"}`} />
         </button>
       </div>
-      {failure != null && <div className="alert-danger" role="alert">{failure}</div>}
+      {failure != null && (
+        <div className="alert-danger" role="alert">
+          {failure}
+        </div>
+      )}
     </div>
-  )
-
+  );
 }
 
-function FilterableFacet({Name, Value, link}) {
-  const short = Name.length > 32 ? `${Name.substring(0, 32)}...` : Name
-  return <li>
-    {link === null ? short : <a className='link-primary app-link'
-                                href={link.replace('$', Name)}>
-      {short}
-    </a>} <sup>{Value}</sup>
-  </li>
+function FilterableFacet({ Name, Value, link }) {
+  const short = Name.length > 32 ? `${Name.substring(0, 32)}...` : Name;
+  return (
+    <li>
+      {link === null ? (
+        short
+      ) : (
+        <a className="link-primary app-link" href={link.replace("$", Name)}>
+          {short}
+        </a>
+      )}{" "}
+      <sup>{Value}</sup>
+    </li>
+  );
 }
 
-export function SearchFacet({name, items, link = null}) {
-  let result = []
+export function SearchFacet({ name, items, link = null }) {
+  let result = [];
   if (items.length > 1) {
-    result.push(<div key={name} className='search-facet'>
-      <strong>{name}</strong>
-      <ul>
-        {items.map(f => <FilterableFacet key={f.Name} link={link} {...f} />)}
-      </ul>
-    </div>)
+    result.push(
+      <div key={name} className="search-facet">
+        <strong>{name}</strong>
+        <ul>
+          {items.map(f => (
+            <FilterableFacet key={f.Name} link={link} {...f} />
+          ))}
+        </ul>
+      </div>
+    );
   }
-  return result
+  return result;
 }
 
-function QueryFacetFilter({Name, Value, Filter, endpoint}) {
-  const short = Name.length > 32 ? `${Name.substring(0, 32)}...` : Name
-  const link = Name !== "n/a" ? `${endpoint}?filter=${Filter}` : null
-  return <li>
-    {link === null ? short : <a className='link-primary app-link' href={link}>
-      {short}
-    </a>} <sup>{Value}</sup>
-  </li>
+function QueryFacetFilter({ Name, Value, Filter, endpoint }) {
+  const short = Name.length > 32 ? `${Name.substring(0, 32)}...` : Name;
+  const link = Name !== "n/a" ? `${endpoint}?filter=${Filter}` : null;
+  return (
+    <li>
+      {link === null ? (
+        short
+      ) : (
+        <a className="link-primary app-link" href={link}>
+          {short}
+        </a>
+      )}{" "}
+      <sup>{Value}</sup>
+    </li>
+  );
 }
 
-function QueryFacet({Name, Top, endpoint}) {
-  let result = []
+function QueryFacet({ Name, Top, endpoint }) {
+  let result = [];
   if (Top.length > 1) {
-    result.push(<div key={Name} className='search-facet'>
-      <strong>{Name}</strong>
-      <ul>
-        {Top.map(f => 
-          <QueryFacetFilter key={f.Name} endpoint={endpoint} {...f} />)}
-      </ul>
-    </div>)
+    result.push(
+      <div key={Name} className="search-facet">
+        <strong>{Name}</strong>
+        <ul>
+          {Top.map(f => (
+            <QueryFacetFilter key={f.Name} endpoint={endpoint} {...f} />
+          ))}
+        </ul>
+      </div>
+    );
   }
-  return result
+  return result;
 }
 
-export function QueryFacets({Facets, endpoint}) {
-  return Facets != null && Facets.map(f => 
-    <QueryFacet key={f.Name} endpoint={endpoint} {...f} />)
+export function QueryFacets({ Facets, endpoint }) {
+  return Facets != null && Facets.map(f => <QueryFacet key={f.Name} endpoint={endpoint} {...f} />);
 }
 
 export function useInterval(callback, delay) {
-	// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-	const savedCallback = useRef();
-	useEffect(() => {
-		savedCallback.current = callback;
-	});
-	useEffect(() => {
-		function tick() {
-			savedCallback.current();
-		}
-		if (delay !== null) {
-			let id = setInterval(tick, delay);
-			return () => clearInterval(id);
-		}
-	}, [delay]);
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  const savedCallback = useRef();
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
 
 export function useTitle(title) {

571456e2185c5ea8b6d3314506d0f6b1c38a555a
Sriram Keerthi Madhava Kunjathur <shramk@gmail.com>
2023-07-26 22:16:32 +0200

    Change proxy target host to 127.0.0.1

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://0.0.0.0:8089",
+        target: "http://127.0.0.1:8089",
         changeOrigin: true,
         secure: false
       }

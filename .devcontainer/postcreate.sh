d5f37a3525b8618044fa1415541705cb5460704f
Aviad Hahami <ahahami@paloaltonetworks.com>
2024-02-25 11:45:03 +0200

    chore: allow usage for when behind self-signed certs

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 NVM_VERSION="0.38.0"
 NODE_VERSION="20"
+
+# # If you're developing in a container behind a VPNed/MDM-managed machine you might get "self signed certs in chain" error.
+# # This snippet below bypasses that (ssl is required later for nvm installation)
+# apt update && apt install -y git
+# git config --global http.sslVerify false
 
 echo "[ + ] Running post-create script. Installing the following:"
 echo "[ + ] NVM version: $NVM_VERSION"
@@ -11,7 +15,7 @@ echo "[ + ] =============================="
 # Check if we don't have nvm, if no - install it
 echo "[ + ] Installing nvm at v$NVM_VERSION"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
+curl -k -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
 # Activate nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
